f6b6e7de93d2f4f9d366e13f7d93af9efa1bc9d8
Leonardo Dias <leonardodias@ucl.br>
2021-08-05 16:28:36 -0300

    Add function to count the number of items get from the API. Add test to check the count function

--- src/api_meal.js
+++ src/api_meal.js
@@ -1,5 +1,6 @@
 import { homepageFunction } from './homepage.js';
 import { commentsFunction } from './comments.js';
+import { itemsCounter } from './api_like.js';
 
 const fetchURL = 'https://www.themealdb.com/api/json/v1/1/search.php?f=b';
 
@@ -11,6 +12,7 @@ const mealGet = async () => {
     .then((data) => {
       homepageFunction(data.meals);
       commentsFunction(data.meals);
+      itemsCounter(data.meals);
     });
 };
 // eslint-disable-next-line
